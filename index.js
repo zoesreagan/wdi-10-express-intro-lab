502642d1d4f7040a489dbb74a20bbcaea58c248d
Zoe <zoereagan@gmail.com>
2018-04-09 11:46:08 -0500

    commit 2: tip calculator express application created

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ app.get('/greeting/', (req, res) => {
 
 const name = "";
 app.get('/greeting/:name', (req, res) => {
-  res.send("hi there, " + req.params.name)
+  res.send("Hi there, " + req.params.name)
 });
 
 
 app.listen(3000, () => {
   console.log("server is listening on Port 3000");
+});
+
+//Tip Calculator
+
+app.get('/tip/:total/:tip', (req, res) => {
+  const total = req.params.total;
+  const tipPercentage = req.params.tip;
+  const tip = total * (tipPercentage/total)
+  res.send("Tip = " + tip)
 });
